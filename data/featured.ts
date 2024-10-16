a03d5dbb28e21412b3c608fe47ff902802e996cd
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-10-16 17:19:14 +0530

    Featured Card Header done

---
+++ data/featured.ts
@@ -0,0 +1,31 @@
+const featuredData = [
+  {
+    title: "Okarys",
+    logo: "https://example.com/logo-okarys.png", // Placeholder for the logo URL
+    tag: "Feb 2024",
+    video:
+      "https://cdn.dribbble.com/userupload/3743863/file/original-ed64ce215ecf5f88d0c91cf2a4ecb70a.mp4",
+    credits: "Julia Dmitrievna",
+    link: "https://dribbble.com/shots/19587956-Okarys-Brand-Identity",
+  },
+  {
+    title: "Social Media Video App",
+    logo: "https://example.com/logo-socialmediaapp.png", // Placeholder for the logo URL
+    tag: "Dec 2023",
+    video:
+      "https://cdn.dribbble.com/userupload/13004443/file/original-abd310aef7e55087b74367a7d60db3af.mp4",
+    credits: "Julia Dmitrievna",
+    link: "https://dribbble.com/shots/23626219-Social-Media-Video-App",
+  },
+  {
+    title: "New Branding Project",
+    logo: "https://example.com/logo-brandingproject.png", // Add another item for variety
+    tag: "Mar 2024",
+    video:
+      "https://cdn.dribbble.com/userupload/3743863/file/original-newbrandingproject.mp4",
+    credits: "Alex Johnson",
+    link: "https://dribbble.com/shots/3743863-New-Branding-Project",
+  },
+];
+
+export default featuredData;
--- sections/featured.tsx
+++ sections/featured.tsx
@@ -1,11 +1,22 @@
-import Heading from "@/components/Heading/Heading";
-import React from "react";
+import FeaturedCard from "@/components/cards/featured/featured-card";
+import Heading from "@/components/Heading/heading";
+import { featuredData } from "@/data";
+
+const MainFeatured = featuredData[0];
 
 const Featured = () => {
   return (
     <div className="pt-24 px-3 lg:px-8">
       {/* Heading */}
       <Heading number="01" title_1="Featured" title_2="Work" />
+
+      {/* Main  Featured Card */}
+      <FeaturedCard
+        active
+        title={MainFeatured.title}
+        tag={MainFeatured.tag}
+        video={MainFeatured.video}
+      />
     </div>
   );
 };
